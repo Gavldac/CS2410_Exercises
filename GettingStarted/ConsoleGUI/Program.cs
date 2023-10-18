b719922f68a04fa607ef6e53191d058b148260ef
Gavldac <75696834+Gavldac@users.noreply.github.com>
2023-10-18 11:19:48 -0600

    Initial commit

---
+++ GettingStarted/ConsoleGUI/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+
+Console.WriteLine("Console GUI");
+
+int size = 10; //Number of rows in the square
+int topMargin = 5;
+int leftMargin = 10;
+//ConsoleColor color = ConsoleColor.Blue; //Set the initial background color
+
+//Save background
+ConsoleColor background = Console.BackgroundColor;
+
+//Padding
+Console.WriteLine();
+
+// Show a Title
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Title");
+
+//MakeSquare(10, 15, 10, ConsoleColor.DarkCyan);
+
+// Make a series of squares
+for (int i = 0; i < size; i++)
+{
+    Console.BackgroundColor = (Console.BackgroundColor == ConsoleColor.Blue) ? ConsoleColor.Cyan : ConsoleColor.Blue;
+    MakeSquare(topMargin + i, leftMargin + 2 * i, size - 2 * i, Console.BackgroundColor);
+}
+
+
+// Padding
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine();
+}
+
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Black;
+//Console.BackgroundColor = background;
+
+
+
+
+static void MakeSquare(int top, int leftMargin, int size, ConsoleColor color)
+{
+    Console.BackgroundColor = color;
+    Console.CursorTop = top;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.CursorLeft = leftMargin;
+        Console.WriteLine($"{{0, {size * 2}}}", " ");
+    }
+}
---
+++ GettingStarted/Exercises/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.CursorTop = 5;
+
+// Save background color
+ConsoleColor background = Console.BackgroundColor;
+Console.BackgroundColor = ConsoleColor.Blue;
+
+for(int i = 0; i < 5; i++)
+{
+    Console.CursorLeft = 10;
+    Console.WriteLine("                   ");
+
+}
+
+Console.CursorTop = 7;
+Console.CursorLeft = 14;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Hello World");
+
+
+
+//=========
+Console.BackgroundColor = background;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.CursorTop += 3;
+
